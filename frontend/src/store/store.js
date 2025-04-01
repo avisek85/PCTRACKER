dcc0912cce57d54a042143831bbdb486a2851b83
avisek85 <Abhisheksinghrajput5888@gmail.com>
2025-04-01 14:55:26 +0530

    Updated code with more changes

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,10 +1,8 @@
 import {configureStore} from "@reduxjs/toolkit";
-import activityReducer from "./activitySlice";
 import apiReducer from "./apiSlice";
 
  const store = configureStore({
     reducer:{
-        activity:activityReducer, // Local activity data
         api: apiReducer              // Backend communication
     },
 })
